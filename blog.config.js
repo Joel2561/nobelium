ed92d3779ee7ce93603dd8cef113c43cd5229e3c
yu jiale <77143461+Joel2561@users.noreply.github.com>
2021-11-15 21:32:10 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,9 +1,9 @@
 const BLOG = {
-  title: 'CRAIGARY',
-  author: 'Craig Hart',
-  email: 'i@craigary.net',
-  link: 'https://nobelium.vercel.app',
-  description: 'This gonna be an awesome website.',
+  title: 'Joelhub',
+  author: 'Yu jiale',
+  email: 'yujiale2561@163.com',
+  link: 'https://joelhub.xyz',
+  description: 'Words worth sharing.',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   appearance: 'auto', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
@@ -11,7 +11,7 @@ const BLOG = {
   darkBackground: '#18181B', // use hex value, don't forget '#'
   path: '', // leave this empty unless you want to deploy Nobelium in a folder
   since: 2021, // If leave this empty, current year will be used.
-  postsPerPage: 7,
+  postsPerPage: 15,
   sortByDate: false,
   showAbout: true,
   showArchive: true,
